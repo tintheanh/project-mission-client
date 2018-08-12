4ba7241c1827e36ffb18b76d8e115a84658ff9a8
Anh Nguyen <anhnguyen@Anhs-MacBook-Pro.local>
2018-08-11 21:59:22 -0700

    add footer

--- src/components/HomeScreen/Home.js
+++ src/components/HomeScreen/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import Header from './Header';
 import RequestForm from '../RequestSection/RequestForm';
 
@@ -9,8 +10,22 @@ const Home = props => (
     <RequestForm
       subjects={props.subjects}
     />
+    <footer style={styles.footerStyle}>{`© ${moment().year()} Anh Nguyen ・ Nguyen Dinh ・ Nhat Trinh`}</footer>
   </div>
 );
+
+const styles = {
+  footerStyle: {
+    width: '100%',
+    paddingTop: '8px',
+    paddingBottom: '8px',
+    position: 'absolute',
+    bottom: '0',
+    color: '#7F7E7E',
+    textAlign: 'center',
+    background: '#464646'
+  }
+};
 
 Home.propTypes = {
   subjects: PropTypes.arrayOf(PropTypes.object).isRequired
